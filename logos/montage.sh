07dbcd2fa7a22fa2229a3804ccf89079b5eaaac7
Jarrell Waggoner <malloc47@gmail.com>
2013-03-25 01:18:29 -0400

    logos and script to generate montage of them

---
+++ logos/montage.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+montage -tile 3x4 -geometry 256x128 instagram.png pinterest.jpg rdio.png disqus.png mozilla.png bitbucket.jpg lanyrd.gif mahalo.jpg guardian.jpg washington-post.jpg new-york-times.jpg the-onion.png ../img/montage.png
